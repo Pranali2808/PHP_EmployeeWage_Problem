e07073b5b78249fba93dd816c600c0ab982ddec9
pranali2808 <ranepranali1997@gmail.com>
2022-06-27 14:28:28 +0530

    Calculate the daily wage of employee

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -1,23 +1,23 @@
 <?php
-// UC1 : Program for Employee Attendence 
- //Check Employee Present or not
- 
-       echo "**********Welcome to Employee Wage Computation Problem************";
+// UC2 : Check Employee Present or not
+//calculate employee daily wage according to emp attendance
+       echo "**********Welcome to Employee Wage Computation Problem************\n";
 
-class EmployeeWage
-{
+       $IS_PRESENT = 1;
+       $EMPWAGE_PER_HOUR = 20;
+       $EMP_HOURS;
+        
+       $Num = rand(0, 1);//function to generate random number 0 or 1 for Attendance
+       if ($Num == $IS_PRESENT ) {
+           echo "Employee is present\n";
+           $EMP_HOURS = 8;
+           
+       } else {
+           echo "Employee is absent\n";
+           $EMP_HOUR = 0;
+           
+       }
+       $EmpDailyWage = $EMP_HOURS * $EMPWAGE_PER_HOUR;
+       echo "Employee Daily Wage is : $EmpDailyWage \n";
 
-    function Attendance()//function to check employee is present or not
-    {
-        $empCheck = rand(0, 1);//function to generate random number 0 or 1 for Attendance
-        if ($empCheck == 1) {
-            echo "\nEmployee is present";
-        } else {
-            echo "\nEmployee is absent";
-        }
-    }
-}
-//creating object of class
-$empWage = new EmployeeWage();
-$empWage->Attendance();
 ?>

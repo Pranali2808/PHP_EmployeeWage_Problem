1eb81bc7ffaf47f1b96f249d1cdcb4e428dff7b7
pranali2808 <ranepranali1997@gmail.com>
2022-06-27 15:46:13 +0530

    Calculate Employee Monthly Wage

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -1,12 +1,17 @@
 <?php
-// UC4 : Use Switch Case statement
+// UC5 : Calculate employee monthly wage
 //Check Employee fulltime Present, part time present or absent
 //calculate employee daily wage according to emp attendance
        echo "**********Welcome to Employee Wage Computation Problem************\n";
 
+       $WORKING_DAY_PER_MONTH = 20;
        $EMPWAGE_PER_HOUR = 20;
        $EMP_HOURS;
-        
+       $EmpMonthlyWage = 0;
+      
+       for($i=1 ; $i <= $WORKING_DAY_PER_MONTH; $i++)
+       {
+       
        $Num = rand(0, 2);//function to generate random number 0 or 2 for Attendance
        switch($Num){
         case 0 :
@@ -22,7 +27,11 @@
             $EMP_HOURS = 0;
             break;
         }
-       $EmpDailyWage = $EMP_HOURS * $EMPWAGE_PER_HOUR;
-       echo "Employee Daily Wage is : $EmpDailyWage \n";
+        $EmpDailyWage = $EMP_HOURS * $EMPWAGE_PER_HOUR;
+      
+        $EmpMonthlyWage += $EmpDailyWage;
+      
+    }
+    echo "Employee Monthly Wage is : $EmpMonthlyWage \n";
 
 ?>

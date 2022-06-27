51f551e6d8e0701f5b6b4ba39c7b95d6d9ba5694
pranali2808 <ranepranali1997@gmail.com>
2022-06-27 15:08:00 +0530

    Calculate daily wage of employee using switch case statement

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -1,25 +1,26 @@
 <?php
-// UC3 : Check Employee fulltime Present, part time present or absent
+// UC4 : Use Switch Case statement
+//Check Employee fulltime Present, part time present or absent
 //calculate employee daily wage according to emp attendance
        echo "**********Welcome to Employee Wage Computation Problem************\n";
 
-       $IS_FULLTIME_PRESENT = 1;
-       $IS_PARTTIME_PRESENT = 2;
        $EMPWAGE_PER_HOUR = 20;
        $EMP_HOURS;
         
        $Num = rand(0, 2);//function to generate random number 0 or 2 for Attendance
-       if ($Num == $IS_FULLTIME_PRESENT ) {
-          // echo "Employee is full time present\n";
-           $EMP_HOURS = 8;
-       }
-       elseif($Num == $IS_PARTTIME_PRESENT){
-              //echo "Employee is part time present\n";
-              $EMP_HOURS = 4;
-       }
-        else {
-           //echo "Employee is absent\n";
-           $EMP_HOUR = 0;
+       switch($Num){
+        case 0 :
+            //echo "Employee full time present";
+            $EMP_HOURS = 8;
+            break;
+        case 1 :
+           // echo "Employee Part time present";
+            $EMP_HOURS = 4;
+            break;
+        default :
+            //echo "Employee is absent";
+            $EMP_HOURS = 0;
+            break;
         }
        $EmpDailyWage = $EMP_HOURS * $EMPWAGE_PER_HOUR;
        echo "Employee Daily Wage is : $EmpDailyWage \n";

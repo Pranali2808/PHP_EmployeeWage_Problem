7cb0ca25a4c86a38609333689af3861eea0e33f6
pranali2808 <ranepranali1997@gmail.com>
2022-06-28 22:16:06 +0530

    employeewage problem using class method

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -1,17 +1,23 @@
 <?php
-// UC6 : Calculate Wages till a condition of total working hours or days is reached for Calculate employee monthly wage
+// UC7 :solve employeewage problem using class object method
+//Calculate Wages till a condition of total working hours or days is reached for Calculate employee monthly wage
 //Check Employee fulltime Present, part time present or absent
 //calculate employee daily wage according to emp attendance
-echo "**********Welcome to Employee Wage Computation Problem************\n";
-$WORKING_HOURS_PER_MONTH = 100;
-$WORKING_DAY_PER_MONTH = 20;
-$EMPWAGE_PER_HOUR = 20;
-$EMP_HOURS;
-$TOTAL_EMP_HOURS = 0;
-$EmpMonthlyWage = 0;
-$TOTAL_WORKING_DAY = 1;
 
-          while($TOTAL_WORKING_DAY <= $WORKING_DAY_PER_MONTH && $TOTAL_EMP_HOURS <= $WORKING_HOURS_PER_MONTH)
+echo "**********Welcome to Employee Wage Computation Problem************\n";
+class EmployeeWage
+{
+
+public $WORKING_HOURS_PER_MONTH = 100;
+public $WORKING_DAY_PER_MONTH = 20;
+public $EMPWAGE_PER_HOUR = 20;
+public $EMP_HOURS;
+public $TOTAL_EMP_HOURS = 0;
+public $EmpMonthlyWage = 0;
+public $TOTAL_WORKING_DAY = 1;
+ 
+         public function MonthlyWage(){
+          while($this->TOTAL_WORKING_DAY <= $this->WORKING_DAY_PER_MONTH && $this->TOTAL_EMP_HOURS <= $this->WORKING_HOURS_PER_MONTH)
           {
 
             $Num = rand(0, 2);//function to generate random number 0 or 2 for Attendance
@@ -29,17 +35,18 @@ $TOTAL_WORKING_DAY = 1;
              $EMP_HOURS = 0;
               break;
             }
-     $TOTAL_EMP_HOURS += $EMP_HOURS;
-     $TOTAL_WORKING_DAY ++ ;
+     $this->TOTAL_EMP_HOURS += $EMP_HOURS;
+     $this->TOTAL_WORKING_DAY ++ ;
  
-     $EmpDailyWage = $EMP_HOURS * $EMPWAGE_PER_HOUR;
+     $EmpDailyWage = $EMP_HOURS * $this->EMPWAGE_PER_HOUR;
 
-       $EmpMonthlyWage += $EmpDailyWage;
-
+       $this->EmpMonthlyWage += $EmpDailyWage;
+          }
+          echo "Employee Monthly Wage is : $this->EmpMonthlyWage \n";
+       }
 }
 
-echo "Employee Monthly Wage is : $EmpMonthlyWage \n";
-
-
+$EmpWage = new EmployeeWage();
+$EmpWage->MonthlyWage();
 ?>
 

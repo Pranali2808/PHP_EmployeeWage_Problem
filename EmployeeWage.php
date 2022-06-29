bebfffcd3e8f5b83a80d8aab86178fe305268f3d
pranali2808 <ranepranali1997@gmail.com>
2022-06-29 11:20:08 +0530

    calculate employee monthlywage using class method

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -8,16 +8,16 @@ echo "**********Welcome to Employee Wage Computation Problem************\n";
 class EmployeeWage
 {
 
-public $WORKING_HOURS_PER_MONTH = 100;
-public $WORKING_DAY_PER_MONTH = 20;
+public $MAX_WORKING_HOURS_PER_MONTH = 100;
+public $MAX_WORKING_DAY_PER_MONTH = 20;
 public $EMPWAGE_PER_HOUR = 20;
 public $EMP_HOURS;
 public $TOTAL_EMP_HOURS = 0;
 public $EmpMonthlyWage = 0;
-public $TOTAL_WORKING_DAY = 1;
+public $TOTAL_WORKING_DAY = 0;
  
-         public function MonthlyWage(){
-          while($this->TOTAL_WORKING_DAY <= $this->WORKING_DAY_PER_MONTH && $this->TOTAL_EMP_HOURS <= $this->WORKING_HOURS_PER_MONTH)
+          function EmpWageMonthly(){
+          while($this->TOTAL_WORKING_DAY <= $this->MAX_WORKING_DAY_PER_MONTH && $this->TOTAL_EMP_HOURS <= $this->MAX_WORKING_HOURS_PER_MONTH)
           {
 
             $Num = rand(0, 2);//function to generate random number 0 or 2 for Attendance
@@ -35,18 +35,18 @@ public $TOTAL_WORKING_DAY = 1;
              $EMP_HOURS = 0;
               break;
             }
-     $this->TOTAL_EMP_HOURS += $EMP_HOURS;
-     $this->TOTAL_WORKING_DAY ++ ;
- 
-     $EmpDailyWage = $EMP_HOURS * $this->EMPWAGE_PER_HOUR;
+            $this->TOTAL_WORKING_DAY ++ ;
+            $this->TOTAL_EMP_HOURS += $EMP_HOURS;
+    
+         $EmpDailyWage = $EMP_HOURS * $this->EMPWAGE_PER_HOUR;
 
-       $this->EmpMonthlyWage += $EmpDailyWage;
+          $this->EmpMonthlyWage += $EmpDailyWage;
           }
-          echo "Employee Monthly Wage is : $this->EmpMonthlyWage \n";
+          echo "Employee Monthly Wage is : $this->EmpMonthlyWage s \n";
        }
 }
 
 $EmpWage = new EmployeeWage();
-$EmpWage->MonthlyWage();
+$EmpWage->EmpWageMonthly();
 ?>
 

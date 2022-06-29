d1619f9f92a560981be64b533d86fe390a1a9f8e
pranali2808 <ranepranali1997@gmail.com>
2022-06-29 08:59:32 +0530

    Calculate monthly wage as per given conditions

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -3,15 +3,15 @@
 //Check Employee fulltime Present, part time present or absent
 //calculate employee daily wage according to emp attendance
 echo "**********Welcome to Employee Wage Computation Problem************\n";
-$WORKING_HOURS_PER_MONTH = 100;
-$WORKING_DAY_PER_MONTH = 20;
+$MAX_WORKING_HOURS_PER_MONTH = 100;
+$MAX_WORKING_DAY_PER_MONTH = 20;
 $EMPWAGE_PER_HOUR = 20;
 $EMP_HOURS;
 $TOTAL_EMP_HOURS = 0;
 $EmpMonthlyWage = 0;
-$TOTAL_WORKING_DAY = 1;
+$TOTAL_WORKING_DAY = 0;
 
-          while($TOTAL_WORKING_DAY <= $WORKING_DAY_PER_MONTH && $TOTAL_EMP_HOURS <= $WORKING_HOURS_PER_MONTH)
+          while($TOTAL_WORKING_DAY <= $MAX_WORKING_DAY_PER_MONTH && $TOTAL_EMP_HOURS <= $MAX_WORKING_HOURS_PER_MONTH)
           {
 
             $Num = rand(0, 2);//function to generate random number 0 or 2 for Attendance

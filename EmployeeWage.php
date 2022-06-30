de0eeeeb08302611ff1c0a64f55933f6a6b83f87
pranali2808 <ranepranali1997@gmail.com>
2022-06-30 07:36:04 +0530

    Calculate monthlywage for multiple company by passing function parameter

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -1,23 +1,22 @@
 <?php
-// UC7 :solve employeewage problem using class object method
-//Calculate Wages till a condition of total working hours or days is reached for Calculate employee monthly wage
+// UC8 :Calculate wage for multiple companies
+//each company has its own wage ,working hours and days as per month
+//solve employeewage problem using parameter instead of class variable
 //Check Employee fulltime Present, part time present or absent
-//calculate employee daily wage according to emp attendance
+//calculate employee daily wage for getting monthlywage
 
 echo "**********Welcome to Employee Wage Computation Problem************\n";
 class EmployeeWage
 {
 
-public $MAX_WORKING_HOURS_PER_MONTH = 100;
-public $MAX_WORKING_DAY_PER_MONTH = 20;
-public $EMPWAGE_PER_HOUR = 20;
 public $EMP_HOURS;
 public $TOTAL_EMP_HOURS = 0;
 public $EmpMonthlyWage = 0;
 public $TOTAL_WORKING_DAY = 0;
  
-          function EmpWageMonthly(){
-          while($this->TOTAL_WORKING_DAY <= $this->MAX_WORKING_DAY_PER_MONTH && $this->TOTAL_EMP_HOURS <= $this->MAX_WORKING_HOURS_PER_MONTH)
+          function EmpWageMonthly($Company_Name, $MAX_WORKING_HOURS_PER_MONTH,$MAX_WORKING_DAY_PER_MONTH,$EMPWAGE_PER_HOUR)
+          {
+          while($this->TOTAL_WORKING_DAY <= $MAX_WORKING_DAY_PER_MONTH && $this->TOTAL_EMP_HOURS <= $MAX_WORKING_HOURS_PER_MONTH)
           {
 
             $Num = rand(0, 2);//function to generate random number 0 or 2 for Attendance
@@ -38,15 +37,16 @@ public $TOTAL_WORKING_DAY = 0;
             $this->TOTAL_WORKING_DAY ++ ;
             $this->TOTAL_EMP_HOURS += $EMP_HOURS;
     
-         $EmpDailyWage = $EMP_HOURS * $this->EMPWAGE_PER_HOUR;
+         $EmpDailyWage = $EMP_HOURS * $EMPWAGE_PER_HOUR;
 
           $this->EmpMonthlyWage += $EmpDailyWage;
           }
-          echo "Employee Monthly Wage is : $this->EmpMonthlyWage s \n";
+          echo "Employee Monthly Wage for $Company_Name is : $this->EmpMonthlyWage \n";
        }
 }
 
 $EmpWage = new EmployeeWage();
-$EmpWage->EmpWageMonthly();
+$EmpWage->EmpWageMonthly("BMW",80,23,30);
+$EmpWage->EmpWageMonthly("TATA",85,25,25);
+$EmpWage->EmpWageMonthly("BIRLA",90,22,35);
 ?>
-
